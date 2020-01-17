e89f089ce4c6d9fff635626b179041b563256022
Nicola <nicola.ba1989@gmail.com>
2020-01-17 17:26:01 +0100

    bonus

---
+++ bonus/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+// ***************************
+    $('.next').click(
+        function() {
+            sliderNext();
+        });
+
+    $('.prev').click(
+        function() {
+            sliderPrev();
+        });
+
+    $(document).keydown(function() {
+        var keyboard = event.which;
+        switch (keyboard) {
+            case 37:
+                sliderPrev();
+                break;
+            case 39:
+                sliderNext();
+                break;
+        };
+    });
+// ***************************
+});
+
+// ***************************
+function sliderNext() {
+    var imageActive = $('img.active');
+    var circleActive = $('i.active');
+    var imageNext = imageActive.next();
+    var circleNext = circleActive.next();
+
+    if (imageActive.hasClass('last')) {
+        imageActive.removeClass('active');
+        circleActive.removeClass('active');
+        $('img.first').addClass('active');
+        $('i.first').addClass('active');
+    } else {
+        imageActive.removeClass('active');
+        circleActive.removeClass('active');
+        imageNext.addClass('active');
+        circleNext.addClass('active');
+    }
+}
+
+function sliderPrev() {
+    var imageActive = $('img.active');
+    var circleActive = $('i.active');
+    var imageNext = imageActive.prev();
+    var circleNext = circleActive.prev();
+
+    if (imageActive.hasClass('first')) {
+        imageActive.removeClass('active');
+        circleActive.removeClass('active');
+        $('img.last').addClass('active');
+        $('i.last').addClass('active');
+    } else {
+        imageActive.removeClass('active');
+        circleActive.removeClass('active');
+        imageNext.addClass('active');
+        circleNext.addClass('active');
+    }
+}
+// ***************************
